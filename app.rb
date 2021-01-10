dd039c140a1c14b061daeb4b36bf76753f2f02d5
mcardona9015 <mcardona9015@gmail.com>
2021-01-09 22:15:02 -0500

    commit tree 2dc33c924af5718ef6f97f79ee9e3e292b62956c

--- app.rb
+++ app.rb
@@ -1,4 +1,24 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+
+    get '/' do
+        "Hello, World"
+    end
+
+    get '/name' do
+        "My name is Michael"
+    end
+
+    get '/hometown' do
+        "My hometown is NYC"
+    end
+
+    get '/favorite-song' do
+        "My favorite song is Elizabeth Addict"
+    end
+
+
+
+
 end
